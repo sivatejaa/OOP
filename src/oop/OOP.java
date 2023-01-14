0204ded86f3022184d705d61fcd7f402a69aec47
sivatejaa <57987686+sivatejaa@users.noreply.github.com>
2023-01-14 12:54:12 -0600

    Intial commit

---
+++ src/oop/OOP.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package oop;
+
+/**
+ *
+ * @author s558636
+ */
+public class OOP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
