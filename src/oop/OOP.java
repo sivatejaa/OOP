f0ba9dc8a7b7e82c95fb172a6cdb5c293f9bc2dc
sivatejaa <57987686+sivatejaa@users.noreply.github.com>
2023-01-14 13:12:29 -0600

    Update OOP.java

--- src/oop/OOP.java
+++ src/oop/OOP.java
@@ -14,7 +14,7 @@ public class OOP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("hai anu");
+       System.out.println("hai shiva");
     }
     
 }
